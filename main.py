2ba42e1539a1b021834a668297606b5bf1eb7677
Thilagavathy-deepa <144236268+Thilagavathy-deepa@users.noreply.github.com>
2023-09-13 19:37:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-class BankAccount:
-
-    def __init__(self, account_number, account_holder_name, account_balance):
-        self.account_number = account_number
-        self.account_holder_name = account_holder_name
-        self._account_balance = account_balance  # private attribute
-
-    def deposit(self, amount):
-        self._account_balance += amount
-
-    def withdraw(self, amount):
-        if amount > self._account_balance:
-            raise ValueError("Insufficient funds")
-        self._account_balance -= amount
-
-    def get_balance(self):
-        return self._account_balance
-
-
-if __name__ == "__main__":
-    account = BankAccount("123456789", "John Doe", 1000)
-
-    account.deposit(500)
-    print("Account balance:", account.get_balance())
-
-    account.withdraw(200)
-    print("Account balance:", account.get_balance())
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+print(factorial(5))
